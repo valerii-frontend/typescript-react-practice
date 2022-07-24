8075a3acad1d9d2f4dd1bc8600d08f177865adc0
Valerii from earth <lvaldev.code@gmail.com>
2022-07-24 10:33:36 +0200

    add styles for components

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,5 +1,7 @@
+import styles from "./TodoItem.module.css";
+
 const TodoItem: React.FC<{ text: string }> = (props) => {
-	return <li>{props.text}</li>;
+	return <li className={styles["todos-item"]}>{props.text}</li>;
 };
 
 export default TodoItem;
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Todo from "../models/todo";
 import TodoItem from "./TodoItem";
+import styles from "./Todos.module.css";
 
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
 	return (
-		<ul>
+		<ul className={styles["todos-list"]}>
 			{props.items.map((item) => (
 				<TodoItem key={item.id} text={item.text} />
 			))}
