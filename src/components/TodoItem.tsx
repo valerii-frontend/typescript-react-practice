31dd6d5836e6d0e9be779a7c1712e9294a599d03
Valerii from earth <lvaldev.code@gmail.com>
2022-07-24 11:32:35 +0200

    add remove handler

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
 import styles from "./TodoItem.module.css";
 
 const TodoItem: React.FC<{ text: string }> = (props) => {
-	return <li className={styles["todos-item"]}>{props.text}</li>;
+	const [removeTodo, setremoveTodo] = useState(false);
+
+	return (
+		<>
+			{!removeTodo && (
+				<li onClick={() => setremoveTodo(true)} className={styles["todos-item"]}>
+					{props.text}
+				</li>
+			)}
+		</>
+	);
 };
 
 export default TodoItem;
