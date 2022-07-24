b295a3284c3514a0c777da2578b04b5cc53b3cd9
Valerii from earth <lvaldev.code@gmail.com>
2022-07-24 11:50:23 +0200

    add complete handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
@@ -6,7 +6,9 @@ import Todo from "./models/todo";
 
 function App() {
 	const [todos, setTodos] = useState<Todo[]>([]);
+
 	const addTodoHandler = (todoText: string) => setTodos((prev) => prev.concat(new Todo(todoText)));
+
 	return (
 		<div className='App'>
 			<NewTodo onAddTodo={addTodoHandler}></NewTodo>
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -4,14 +4,12 @@ import styles from "./TodoItem.module.css";
 const TodoItem: React.FC<{ text: string }> = (props) => {
 	const [removeTodo, setremoveTodo] = useState(false);
 
+	const toggleHandler = () => setremoveTodo((prev) => !prev);
+
 	return (
-		<>
-			{!removeTodo && (
-				<li onClick={() => setremoveTodo(true)} className={styles["todos-item"]}>
-					{props.text}
-				</li>
-			)}
-		</>
+		<li onClick={toggleHandler} className={[styles["todos-item"], removeTodo ? styles.done : ""].join(" ")}>
+			{props.text}
+		</li>
 	);
 };
 
